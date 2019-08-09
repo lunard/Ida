77c3e53f35d3a4db69e65938b99295169d5c4a34
klyse <Klaus.Pruenster@gmail.com>
2019-08-10 00:34:40 +0200

    added unique experiences route (#3)

---
+++ API/NOI_Hackathon_Summer_Edition_2019_API/Models/TouristInformation.cs
@@ -0,0 +1,9 @@
+﻿using NOI_Hackathon_Summer_Edition_2019_API.Models.ViewModels;
+
+namespace NOI_Hackathon_Summer_Edition_2019_API.Controllers {
+	public class TouristInformation
+	{
+		public FaceRecognitionFilter FaceRecognitionFilter { get; set; }
+		public int[] QuestionResponseIds { get; set; }
+	}
+}
---
+++ API/NOI_Hackathon_Summer_Edition_2019_API/Models/UniqueExperiences.cs
@@ -0,0 +1,9 @@
+﻿namespace NOI_Hackathon_Summer_Edition_2019_API.Controllers {
+	public class UniqueExperiences
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string AdditionalInformation { get; set; }
+	}
+}
