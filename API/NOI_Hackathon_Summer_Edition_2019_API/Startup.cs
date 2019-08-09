bc6b76aff8d27d4a582431134726ae7aaf9c5655
Luca Nardelli <lunard@gmail.com>
2019-08-10 00:36:09 +0200

    Disabled CORS

--- API/NOI_Hackathon_Summer_Edition_2019_API/Startup.cs
+++ API/NOI_Hackathon_Summer_Edition_2019_API/Startup.cs
@@ -19,14 +19,28 @@ namespace NOI_Hackathon_Summer_Edition_2019_API
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-		}
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("disableCORS",
+                builder =>
+                {
+                    builder.WithOrigins("*");
+                    builder.WithHeaders("*");
+                    builder.WithMethods("*");
+                });
+            });
+        }
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
 			if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
 
-			app.UseMvc();
+
+            app.UseCors("disableCORS");
+
+            app.UseMvc();
 		}
 	}
 }
