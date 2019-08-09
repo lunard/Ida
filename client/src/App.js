865fec785f16a72e99c30ba3ac6182a9b1896834
nik <nicola.gnecchi@gmail.com>
2019-08-09 20:07:11 +0200

    client v1

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+const version = 1;
+
 function App() {
   return (
     <div className="App">
-      <h1>IDA</h1>
+      <h1>IDA {version}</h1>
       <button onClick={() => window.testElectron()}>test</button>
     </div>
   );
