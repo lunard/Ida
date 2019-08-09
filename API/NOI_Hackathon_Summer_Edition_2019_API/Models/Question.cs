d7df1d4fb3617d23349291f5bfc8fc977cc6d02d
Unknown <Klaus.Pruenster@gmail.com>
2019-08-10 00:32:56 +0200

    added unique experiences route

---
+++ API/NOI_Hackathon_Summer_Edition_2019_API/Models/Question.cs
@@ -0,0 +1,8 @@
+﻿namespace NOI_Hackathon_Summer_Edition_2019_API.Controllers {
+	public class Question
+	{
+		public string Q { get; set; }
+		public Question NextQuestion1 { get; set; }
+		public Question NextQuestion2 { get; set; }
+	}
+}
