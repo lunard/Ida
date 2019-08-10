b597629b87b20f8a37cdcbb10de15db1033a2448
nik <nicola.gnecchi@gmail.com>
2019-08-10 11:22:03 +0200

    itinerary

--- client/src/containers/itinerary.jsx
+++ client/src/containers/itinerary.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import langSrc from '../assets/question.svg';
+import girlSrc from '../assets/girl.png';
 
 const Itinerary = ({ restart, info }) => {
   return (
     <div
       className="choice-page"
       style={{ backgroundImage: `url('${langSrc}')` }}>
-      <h1>Itinerary</h1>
+      <img className="iti-girl" src={girlSrc} />
       <div className="itinerary">
         {info.itinerary &&
           info.itinerary.map(it => (
-            <div key={it.id}>
+            <div className="iti" key={it.id}>
               <h1>{it.name}</h1>
               <p>{it.description}</p>
-              <p>
+              <h2>Advice</h2>
+              <div>
                 <i>{it.additionalInformation}</i>
-              </p>
+              </div>
             </div>
           ))}
       </div>
-      <button>print</button>
-      <button>send to email</button>
-      <button onClick={restart}>restart</button>
     </div>
   );
 };
